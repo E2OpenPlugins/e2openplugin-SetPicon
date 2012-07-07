7a0137ebb15479b0be0598ed37c6b8b0cdafea19
ims <ims21@users.sourceforge.net>
2012-07-07 12:23:46 +0200

    repair

--- setup_translate.py
+++ setup_translate.py
@@ -5,7 +5,6 @@ from distutils.command.build import build as _build
 import os
 
 class build_trans(cmd.Command):
-<<<<<<< HEAD
 	description = 'Compile .po files into .mo files'
 	def initialize_options(self):
 		pass
@@ -35,35 +34,3 @@ cmdclass = {
 	'build': build,
 	'build_trans': build_trans,
 }
-
-=======
-description = 'Compile .po files into .mo files'
-def initialize_options(self):
-pass
-
-def finalize_options(self):
-pass
-
-def run(self):
-s = os.path.join('plugin', 'locale')
-for lang in os.listdir(s):
-lc = os.path.join(s, lang, 'LC_MESSAGES')
-if os.path.isdir(lc):
-for f in os.listdir(lc):
-if f.endswith('.po'):
-src = os.path.join(lc, f)
-dest = os.path.join(lc, f[:-2] + 'mo')
-print "Language compile %s -> %s" % (src, dest)
-if os.system("msgfmt '%s' -o '%s'" % (src, dest)) != 0:
-raise Exception, "Failed to compile: " + src
-
-class build(_build):
-sub_commands = _build.sub_commands + [('build_trans', None)]
-def run(self):
-_build.run(self)
-
-cmdclass = {
-'build': build,
-'build_trans': build_trans,
-}
->>>>>>> c978ca86978cdc722a3b9e7001f4109dc1640e55
