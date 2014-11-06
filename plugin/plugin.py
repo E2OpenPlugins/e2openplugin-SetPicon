638d7f3cb6330d41ef31243939cb45134d544b58
ims21 <martin@volyne.cz>
2014-11-06 10:54:05 +0100

    Update plugin.py
    remove whitespace

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -35,7 +35,7 @@ def main(session, servicelist=None, **kwargs):
 
 def Plugins(path,**kwargs):
 	global plugin_path
-    	plugin_path = path
+	plugin_path = path
 	name= _("SetPicon")
 	descr=_("set picon to service")
 	list = [ PluginDescriptor(name=name, description=descr, where=PluginDescriptor.WHERE_EVENTINFO, needsRestart = False, fnc=main),]
