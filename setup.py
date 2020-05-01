ee065ac4b9cec3620f1f6652a9909e3a55c4ca9b
ims <ims21@users.sourceforge.net>
2020-05-01 10:05:42 +0200

    remove whitespace

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import setup_translate
 
 pkg = 'Extensions.SetPicon'
 setup (name = 'enigma2-plugin-extensions-setpicon',
-       version = '0.53',
-       description = 'assignment picons for services',
-       packages = [pkg],
-       package_dir = {pkg: 'plugin'},
-       package_data = {pkg: ['*.xml', 'setpicon.png', '*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
-       cmdclass = setup_translate.cmdclass, # for translation
-      )
+	version = '0.53',
+	description = 'assignment picons for services',
+	packages = [pkg],
+	package_dir = {pkg: 'plugin'},
+	package_data = {pkg: ['*.xml', 'setpicon.png', '*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
+	cmdclass = setup_translate.cmdclass, # for translation
+	)
